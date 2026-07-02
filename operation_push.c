3aef06e806f1d5796380f4777ecebf9d8a6ee02a
juowou <190452699+juowou@users.noreply.github.com>
2026-07-02 16:39:24 +0100

    modified:   operation_push.c

--- operation_push.c
+++ operation_push.c
@@ -6,43 +6,43 @@
 int operation_push(t_node **stack_a, t_node **stack_b)
 {
     // EXEMPLO:
-    // **stack_a = 1, 2, 3
-    // **stack_b = 4, 5
+    // **stack_a = 1, 2, 3  (3 nodes)
+    // **stack_b = 4, 5     (2 nodes)
     t_node  *tmp;
 
     if (*stack_a == NULL)
         return (0);
 
-    // tmp = o node que queremos copiar para o stack_b (com o valor de stack_a->value)
+    // tmp = novo node, com o valor copiado do stack_a->value
     tmp = create_node((*stack_a)->value);
-    // tmp = 1, tmp->next = NULL
+    // tmp = 1 (stack_a->value), tmp->next = NULL
     if (!tmp)
         return (0);
 
-    // o proximo item do tmp é o atual topo do stack_b
+    // o proximo node do tmp é o atual topo do stack_b
     tmp->next = *stack_b;
-    // tmp = 1, tmp->next = 4 (stack_b)
+    // tmp = 1, tmp->next = 4 (*stack_b)
 
     // o *stack_b passa a apontar para o tmp
     *stack_b = tmp;
-    // *stack_b = 1 (tmp)
+    // *stack_b = 1 (*tmp)
     // **stack_b = 1, 4, 5
     // **stack_a = 1, 2, 3
 
     // STACK_B CONCLUIDO. LIMPAR STACK_A:
     // tmp = atual topo do stack_a (que foi copiado para o stack_b)
     tmp = *stack_a;
-    // tmp = 1 (stack_a)
+    // tmp = 1 (*stack_a)
 
-    // *stack_a passa a apontar para o proximo item (stack_a->next);
+    // *stack_a passa para o proximo item (stack_a->next);
     *stack_a = (*stack_a)->next;
-    // stack_a = 2 (stack_a->next);
+    // stack_a = 2 (*stack_a->next);
 
-    // eliminar o topo do stack_a (tmp) (que foi copiado para o stack_b)
+    // eliminar *tmp (antigo topo do stack_a) (que foi copiado para o stack_b)
     free(tmp);
     // tmp = 1 -> eliminado;
 
-    // **stack_a = 2, 3
-    // **stack_b = 1, 4, 5
+    // **stack_a = 2, 3     (2 nodes)
+    // **stack_b = 1, 4, 5  (3 nodes)
     return (1);
 }
